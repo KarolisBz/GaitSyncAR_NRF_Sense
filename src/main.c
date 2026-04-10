0febc6a030d42af3e3346fa9492865cc0e91207b
Karolis Bandziulis <G00417529@atu.ie>
2026-04-10 11:55:55 +0100

    added external LED aswell as internal

--- src/main.c
+++ src/main.c
@@ -12,22 +12,31 @@ int main(void) {
         return 0;
     }
 
+    // module led pin
     gpio_pin_configure(gpio0, 6, GPIO_OUTPUT_ACTIVE);
 
+    // external pin, p0.29 blue led
+    gpio_pin_configure(gpio0, 29, GPIO_OUTPUT_ACTIVE);
+
+    // Wait for USB console to stabilize
     k_sleep(K_MSEC(3000));
     printk("\n--- GaitSyncAR Booting Up ---\n");
 
+    // Initialize BLE
     if (ble_handler_init() != 0) {
         printk("FATAL: Failed to start BLE!\n");
     }
 
+    // Initialize Battery Monitor
     int err = battery_monitor_init();
     if (err != 0) {
         printk("Battery Monitor Init Failed: %d\n", err);
     }
 
     while (1) {
+        // Toggle leds
         gpio_pin_toggle(gpio0, 6);
+        gpio_pin_toggle(gpio0, 29);
 
         uint32_t batt_mv = battery_monitor_get_mv();
         
